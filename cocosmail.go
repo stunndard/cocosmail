4e56b5d827064c0c6f44f97e416e646efa04c700
Roman Butusov <roman.butusov@gmail.com>
2021-06-26 03:52:58 +0800

    add quick runtime stats

--- cocosmail.go
+++ cocosmail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -192,6 +193,11 @@ func main() {
 				go pop3.NewPop3d(pop3Dsn[0]).ListenAndServe()
 			}
 
+			// runtime stats
+			if core.Cfg.GetDebugEnabled() {
+				go NewMonitor(60)
+			}
+
 			<-sigChan
 			core.Logger.Info("Exiting...")
 
@@ -209,4 +215,49 @@ func main() {
 	}
 	_ = app.Run(os.Args)
 
+}
+
+type Monitor struct {
+	Alloc,
+	TotalAlloc,
+	Sys,
+	Mallocs,
+	Frees,
+	LiveObjects,
+	PauseTotalNs uint64
+	NumGC        uint32
+	NumGoroutine int
+}
+
+func NewMonitor(duration int) {
+	var m Monitor
+	var rtm runtime.MemStats
+	var interval = time.Duration(duration) * time.Second
+	for {
+		<-time.After(interval)
+
+		// Read full mem stats
+		runtime.ReadMemStats(&rtm)
+
+		// Number of goroutines
+		m.NumGoroutine = runtime.NumGoroutine()
+
+		// Misc memory stats
+		m.Alloc = rtm.Alloc
+		m.TotalAlloc = rtm.TotalAlloc
+		m.Sys = rtm.Sys
+		m.Mallocs = rtm.Mallocs
+		m.Frees = rtm.Frees
+
+		// Live objects = Mallocs - Frees
+		m.LiveObjects = m.Mallocs - m.Frees
+
+		// GC Stats
+		m.PauseTotalNs = rtm.PauseTotalNs
+		m.NumGC = rtm.NumGC
+
+		// Just encode to json and print
+		b, _ := json.Marshal(m)
+		core.Logger.Debug(fmt.Sprintf("runtime stats: %s", string(b)))
+	}
 }
